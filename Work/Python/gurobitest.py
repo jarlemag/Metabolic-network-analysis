4694ab87fc42b0d939e508be2a302b26c11db025
Jarle Pahr <jarle.pahr@gmail.com>
2013-10-04 14:38:42 +0200

    Test script for solving FBA problem with Gurobi.

--- Work/Python/gurobitest.py
+++ Work/Python/gurobitest.py
@@ -18,3 +18,55 @@ for v in m.getVars():
     print v.varName, v.x 
 
 print 'Obj:', m.objVal
+
+
+
+#Use gurobi to solve an FBA problem:
+
+from cobra.io.sbml import create_cobra_model_from_sbml_file
+
+cobramodel = create_cobra_model_from_sbml_file('../SBML/SCHUETZR.xml')
+
+
+gurobimodel =gurobi.Model("Schuetz") #Initialize a new Gurobi model
+
+
+#add one decision variable for each reaction in the cobrapy model:
+
+objective = gurobi.LinExpr() 
+variables = []
+for reaction in cobramodel.reactions:
+    newvar = gurobimodel.addVar(lb = reaction.lower_bound, ub = reaction.upper_bound, name = reaction.id) #Add reactions as decision variables, with bounds.
+    objective.add(reaction.objective_coefficient * newvar) #Add the reactions to the objective with their respective objective coefficients.
+    #print reaction.id
+    #variables += newvar
+gurobimodel.update()
+gurobimodel.setObjective(objective) #Apply the objective defined above.
+gurobimodel.update()
+
+gurobimodel.getObjective() #Show the objective
+
+for metabolite in cobramodel.metabolites:
+    reactions = metabolite.get_reaction()
+    '''
+    #newconstr = gurobi.LinExpr()
+    for rxn in reactions:
+        coef = rxn.get_coefficient(metabolite.id)
+        #newconstr.add(coef* gurobimodel.getVarByName(rxn.id))
+    '''
+    newconstr = gurobi.LinExpr([rxn.get_coefficient(metabolite.id) for rxn in reactions],[gurobimodel.getVarByName(rxn.id) for rxn in reactions])
+    gurobimodel.addConstr(newconstr, gurobi.GRB.EQUAL, 0, metabolite.id)    
+        #print coef
+gurobimodel.update()
+
+#Set sense to maximize:
+gurobimodel.modelsense = -1
+gurobimodel.update()
+
+gurobimodel.optimize()
+
+
+gurobimodel.NumVars
+gurobimodel.NumConstrs
+
+f = gurobimodel.optimize()
