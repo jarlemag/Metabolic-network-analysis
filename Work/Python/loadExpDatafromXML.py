a12b133484baab4297e2cc7204ed08a356d2ae74
Jarle Pahr <jarle.pahr@gmail.com>
2014-01-10 09:29:24 +0100

    Loading experimental flues from .xml

--- Work/Python/loadExpDatafromXML.py
+++ Work/Python/loadExpDatafromXML.py
@@ -4,98 +4,29 @@ import xml.etree.ElementTree as ET
 
 #from lxml import etree
 
-tree = ET.parse('experimentaldata.xml')
+tree = ET.parse('expdata.xml')
 root = tree.getroot()
-
+#simple commands:
 #print(root.tag)
 #print(root.attrib)
 #for child in root:
-#    print(child.tag,child.attrib)
+#print(child.tag,child.attrib)
 #root.attrib.keys()
-
 #for element in root:
 #    print(element)
 
-
-def getNodesByType(tree,typeID):
-    return root.findall(typeID) #Return all child nodes of type typeID
-    
-
-
-def selectPublications(tree,identity):
-    pubs = getNodesByType(tree,'publication')
-    hits = []
-    for pub in pubs:
-        if pub.get('id') == identity: #If the the "id" attribute of the node equals the input
-            hits.append(pub) #add the node to the list of hits
-    return hits
-
-hits = selectPublications(root,'Perrenoud')
-
-#Simple format (attribute values only):
-'''
-['Perrenoud, 'Batch','Aerobe'] #
-'''
+def loadExpFluxesfromXML(filename,publication_id,reactor_id,experiment_id):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    string = './/publication[@id="{pub_id}"]/reactor[@id="{react_id}"]/experiment[@id="{exp_id}"]/reactiondata'.format(pub_id = publication_id, react_id = reactor_id, exp_id = experiment_id)
+    print(string)
+    reactiondata = root.findall(string)
+    reactionlist = reactiondata[0].findall('reaction')
+    expfluxlist = [reaction.get('flux') for reaction in reactionlist]
+    expfluxdict = {reaction.get('id'):reaction.get('flux') for reaction in reactionlist}
+    return expfluxdict
 
 
-#List with element names ("publication"), attribute names and attribute values:
-'''
-[Publication  id = "Perrenoud", reactor type = "Batch", experiment id = "aerobe"
-'''
-
-#List of lists including element name and attribute:value dictionaries
-'''
-[['Publication'],{id:"Perrenoud}],["reactor",{type:"Batch"}],["experiment",{id: "aerobe"}]]
-
-'''
-
-def findNodeByAttribute(root,attributes,targetvalues):
-    '''
-    Finds and returns nodes where the  value of the attribute defined by the argument "field" equals 
-    that found in the argument "targets", a list of attribute values.
-    '''
-    print('findNodeByAttribute:')
-    if (type(targetvalues) is str): #If the input target value is not a list (only one value), make it one.
-        targetvalues = [targetvalues]
-    newnode = root
-    print('Starting search for node matching specified attribute values.')
-    print('len(targetvaues)',len(targetvalues))
-    for i in range(0,len(targetvalues)):
-        print('i:',i)
-        value = targetvalues[i]
-        print('Current targetvalue:',value)
-        success = False
-        currentnode = newnode
-        for j in range(0,len(currentnode)):
-            print('j:',j)
-            if currentnode[j].get('id') == targetvalues[i]:
-                print('Found matching attribute value:',value)
-                newnode = currentnode[j]
-                print('New current node:',newnode.tag)
-                success = True
-                           
-        if success == False:
-            print('Failed to find matching node.')
-            return
-    return newnode
-
-#q = findByAttributeValue(root,'id','Perrenoud')
-
-#w = findByAttributeValue(root,'id',','Batch')
-targetvalues = ['Perrenoud','Batch']
-
-z = findNodeByAttribute(root,'id',['Perrenoud','Batch','aerobe'])
-
-#print(z)
+expfluxes = loadExpFluxesfromXML('expdata.xml','Perrenoud','Batch','aerobe')
 
 
-#def getFluxValues(experiment):
-    
-
-reactiondata = root.findall('.//publication[@id="Perrenoud"]/reactor[@id="Batch"]/experiment[@id="aerobe"]/reactiondata')
-
-reactionlist = reactiondata[0].findall('reaction')
-
-expfluxlist = [reaction.get('flux') for reaction in reactionlist]
-
-expfluxdict = {reaction.get('id'):reaction.get('flux') for reaction in reactionlist}
