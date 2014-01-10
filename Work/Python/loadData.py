35e6b2a78bf4fac7246bcb6deac45022edb704c3
Jarle Pahr <jarle.pahr@gmail.com>
2014-01-10 12:58:13 +0100

    expdata.xml reaction IDs. reactiondata.xml. Loading reactionmap from xml.

--- Work/Python/loadData.py
+++ Work/Python/loadData.py
@@ -29,3 +29,31 @@ def ExpFluxesfromXML(filename,publication_id,reactor_id,experiment_id):
 expfluxes = ExpFluxesfromXML('expdata.xml','Perrenoud','Batch','aerobe')
 
 
+def ReactionMapfromXML(filename,publication_id,model_id):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    string = './/publication[@id="{pub_id}"]/model[@id="{mod_id}"]/link'.format(pub_id=publication_id,mod_id=model_id)
+    rawmap = root.findall(string)
+    reactionmap = []
+    for link in rawmap:
+        linkdict = {}
+        exprxn_ids = link.findall('./exprxn')
+        modrxn_ids = link.findall('./modelrxn')
+        exprxnlist = []
+        modrxnlist = []
+        for element in exprxn_ids:
+            exprxn_id = element.get('id')
+            exprxn_coef = element.get('coef')
+            exprxntuple = (exprxn_id,exprxn_coef)
+            exprxnlist.append(exprxntuple)
+        for element in modrxn_ids:
+            modrxn_id = element.get('id')
+            modrxn_coef = element.get('coef')
+            modrxntuple = (modrxn_id,modrxn_coef)
+            modrxnlist.append(modrxntuple)
+        linkdict['expid'] =exprxnlist
+        linkdict['modit'] =modrxnlist
+        reactionmap.append(linkdict)
+    return reactionmap
+    
+#rmap = ReactionMapfromXML('reactionmaps.xml','Perrenoud','SCHUETZR')
