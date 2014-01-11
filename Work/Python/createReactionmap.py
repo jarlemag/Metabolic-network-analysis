819e0a9083f8ed89e195968c660dcee6dfc6e29f
Jarle Pahr <jarle.pahr@gmail.com>
2014-01-11 19:37:02 +0100

    Started on createReactionmap.py (Creating a reaction map .xml file).

---
+++ Work/Python/createReactionmap.py
@@ -0,0 +1,26 @@
+#createReactionmap.py
+
+
+import xml.etree.ElementTree as ET
+
+def createReactionMap(publication_id,model_id,filename, debug = False):
+    '''
+    Writes a reaction map to an .xml file
+    '''
+    root = ET.Element('xml')
+    publication = ET.SubElement(root,'publication',attrib = {'id':publication_id})
+    model = ET.SubElement(publication,'model',attrib={'id':model_id})
+    if debug == True:
+        ET.dump(root)
+    tree = ET.ElementTree(root)
+
+    tree.write(str(filename))
+    return
+
+
+if __name__ == "__main__":
+
+    publication_id = 'Perrenoud'
+    model_id = 'SCHUETZR'
+    filename = "testmap.xml"
+    createReactionMap(publication_id,model_id,filename)
