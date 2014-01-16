7c173cd26af1a4ad322272225a4b0bf1ce46c223
Jarle Pahr <jarle.pahr@gmail.com>
2014-01-16 17:12:02 +0100

    Some pseudocode and function skeletons. Added split ratio data to expdata.xml.

--- Work/Python/loadData.py
+++ Work/Python/loadData.py
@@ -28,7 +28,20 @@ def ExpFluxesfromXML(filename,publication_id,reactor_id,experiment_id,vector = F
         expfluxdict = {reaction.get('id'):float(reaction.get('flux')) for reaction in reactionlist}
     return expfluxdict
 
-expfluxes = ExpFluxesfromXML('expdata.xml','Perrenoud','Batch','aerobe')
+
+def ExpSplitsfromXML(filename,publication_id,reactor_id,experiment_id):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    string = './/publication[@id="{pub_id}"]/reactor[@id="{react_id}"]/experiment[@id="{exp_id}"]/splitsdata'.format(pub_id = publication_id, react_id = reactor_id, exp_id = experiment_id)
+    splitsdata = root.findall(string)
+    splitslist = splitsdata[0].findall('split')
+
+    expsplitsdict = {split.get('id'):float(split.get('flux')) for split in splitslist}
+    return expsplitsdict
+    pass
+
+def ExpSplitsfromTXTtoXML(source,destination,publication_id,reactor_id,experiment_id):
+    pass
 
 def ExpErrorsfromXML(filename,publication_id,reactor_id,experiment_id):
     tree = ET.parse(filename)
@@ -75,17 +88,62 @@ def ReactionMapfromXML(filename,publication_id,model_id):
     return reactionmap
 
 
+class ReactionMap:
+    def __init__(self,linkdicts):
+        self.links = linkdicts
+
 def dictmapToList(dictmap,cobramodel):
     pass
 
 
+class SBMLModel:
+    def __init__(self,metabolites,reactions):
+        self.metabolites = []
+        self.reactions = []
+
+    def add_metabolites(self,metabolitelist):
+        #Check if metabolites are already in model.
+        self.metabolites += metabolitelist #Add metabolites to model
+        
+
+
+class CMetabolite:
+    def __init__(self,ident,name,compartment):
+        self.id = ident
+        self.name = name
+        self.compartment = compartment
+    def get_reactions(self):
+        pass
+
+
+def ModelfromSBML(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    string = './/listOfSpecies'
+    species = root.findall(string)
+
+    metabolitelist = []
+    pass
+
 def XMLtoGurobi(filename):
     #Create a Gurobi model directly from an FBA model saved as SBML
-    pass
+    #pseudocode:
+    '''
+    Load XML file as ElementTree
+    Grab node "list of species"
+    Make an SBMLmodel class instance.
+    (To be continued)
+    
+    
+    '''
 
 if __name__ == "__main__":
     
     rmap = ReactionMapfromXML('reactionmaps.xml','Perrenoud','SCHUETZR')
+
+    expfluxes = ExpFluxesfromXML('expdata.xml','Perrenoud','Batch','aerobe')
+
+    expsplits = ExpSplitsfromXML('expdata.xml','Perrenoud','Batch','aerobe') #Should create a publication "Schuetz" and move splits there.
 
 
 
