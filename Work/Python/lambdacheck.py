6d4e4b62dafd9000c6ff0b9ab48ce4caf5b6a907
Jarle P <jarle.pahr@gmail.com>
2019-06-30 19:39:39 +0200

    Updated print syntax in lambdacheck.py.

--- Work/Python/lambdacheck.py
+++ Work/Python/lambdacheck.py
@@ -5,12 +5,12 @@ S = np.array([[1,-1,0,0,0,0],[0,1,-1,-1,0,0],[0,0,2,0,-1,0],[0,0,0,1,1,-1]])
 
 x = [1,1,1,1,1,7]
 
-print 'Test of row-wise matrix multiplication (vector multiplication).'
+print('Test of row-wise matrix multiplication (vector multiplication).')
 
-print 'Using for loop:'
+print('Using for loop:')
 for i,row in enumerate(S):
     dx = sum(np.multiply(row,x))
-    print 'row:',i, 'row * x:',dx
+    print('row:',i, 'row * x:',dx)
 
 #lambdas = [(lambda x : sum(np.multiply(row,x))) for row in S]
 
@@ -20,7 +20,7 @@ for row in S:
 
 #lambdas = [(lambda row: lambda x: sum(np.multiply(row, x)))(row)]
 
-print 'Using list of lambda functions:'
+print('Using list of lambda functions:')
 for i,fun in enumerate(lambdas):
     dx = fun(x)
-    print 'row:',i,'f(x):',dx
+    print('row:',i,'f(x):',dx)
