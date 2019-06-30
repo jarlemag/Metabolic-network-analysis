4adf1c604d18febc89b2ad9b37489ead752eee64
Jarle P <jarle.pahr@gmail.com>
2019-06-30 18:18:32 +0200

    Added missing ':' in function definition.

--- Work/Python/addtodict.py
+++ Work/Python/addtodict.py
@@ -3,7 +3,7 @@
 Scans two dictionaries, A (targetdict) and B (defaultdict).
 If an element in B is not in A, the element is added to A.
 '''
-def addtodict(targetdict,defaultdict)
+def addtodict(targetdict,defaultdict):
     tempdict = targetdict
     for key in defaultdict:
         if (key in targetdict) == False:
